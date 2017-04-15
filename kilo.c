d909294de9eb264c45ecdb377aee579457373018
= <joao.silva@springernature.com>
2017-04-15 21:00:55 +0200

    Second step: disabled ctrl-s and ctrl-q

--- kilo.c
+++ kilo.c
@@ -1,7 +1,34 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+    struct termios raw = orig_termios;
+    raw.c_iflag &= ~(IXON);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+    enableRawMode();
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1);
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) { //tests whether a character is a control character
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
     return 0;
 }
+
+//next step : Disable Ctrl-V
